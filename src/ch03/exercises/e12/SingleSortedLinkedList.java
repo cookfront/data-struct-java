c58e5f95a7b540eafb862384a2861f57de8f05b5
cookfront <cookfront@gmail.com>
2017-05-14 23:45:27 +0800

    single sorted linked list

--- src/ch03/exercises/e12/SingleSortedLinkedList.java
+++ src/ch03/exercises/e12/SingleSortedLinkedList.java
@@ -1,49 +1,50 @@
-package ch03.exercises.e11;
+package ch03.exercises.e12;
 
 /**
- * Created by cookfront on 2017/5/10.
+ * Created by cookfront on 2017/5/14.
  */
-public class SingleLinkedList<AnyType> {
-    private static class Node<AnyType> {
-        public AnyType data;
-        public Node<AnyType> next;
+public class SingleSortedLinkedList {
+    private static class Node<Comparable> {
+        public Comparable data;
+        public Node<Comparable> next;
 
         public Node() {
             this(null, null);
         }
 
-        public Node(AnyType data) {
+        public Node(Comparable data) {
             this(data, null);
         }
 
-        public Node(AnyType data, Node<AnyType> next) {
+        public Node(Comparable data, Node<Comparable> next) {
             this.data = data;
             this.next = next;
         }
     }
 
-    private Node<AnyType> head;
+    private Node<Comparable> head;
     private int theSize;
 
-    public SingleLinkedList() {
+    public SingleSortedLinkedList() {
         theSize = 0;
-        head = new Node<AnyType>();
+        head = new Node<Comparable>();
         head.next = null;
     }
 
-    public boolean contains(AnyType x) {
-        Node<AnyType> p = head.next;
-        while (p != null) {
+    public boolean contains(Comparable x) {
+        Node<Comparable> p = head.next;
+        while (p != null && p.data.compareTo(x) <= 0) {
             if (x.equals(p.data)) {
                 return true;
+            } else {
+                p = p.next;
             }
-            p = p.next;
         }
         return false;
     }
 
     public void print() {
-        Node<AnyType> p = head.next;
+        Node<Comparable> p = head.next;
         while (p != null) {
             System.out.print(p.data + " ");
             p = p.next;
@@ -55,44 +56,51 @@ public class SingleLinkedList<AnyType> {
         return theSize;
     }
 
-    public boolean add(AnyType x) {
+    public boolean add(Comparable x) {
         if (contains(x)) {
             return false;
         }
 
-        Node<AnyType> p = new Node<>(x, head.next);
-        head.next = p;
+        Node<Comparable> prev = head;
+        Node<Comparable> p = head.next;
+        while (p != null && p.data.compareTo(x) < 0) {
+            prev = p;
+            p = p.next;
+        }
+
+        Node<Comparable> node = new Node<Comparable>(x);
+        prev.next = node;
+        node.next = p;
         theSize++;
 
         return true;
     }
 
-    public boolean remove(AnyType x) {
+    public boolean remove(Comparable x) {
         if (!contains(x)) {
             return false;
         }
 
-        Node<AnyType> prev = head;
-        Node<AnyType> p = head.next;
-
-        while (!p.data.equals(x)) {
+        Node<Comparable> prev = head;
+        Node<Comparable> p = head.next;
+        while (p != null && !p.data.equals(x)) {
             prev = p;
             p = p.next;
         }
+
         prev.next = p.next;
         theSize--;
-
         return true;
     }
 
     public static void main(String ...args) {
-        SingleLinkedList<Integer> testList = new SingleLinkedList<>();
+        SingleSortedLinkedList testList = new SingleSortedLinkedList();
 
         System.out.println("list size: " + testList.size());
 
-        testList.add(1);
         testList.add(2);
         testList.add(3);
+        testList.add(1);
 
         testList.print();
 
